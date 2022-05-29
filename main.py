8c8b733ea0004c401f04e56eab55127a56571b91
Mateus Franco <matsfranco@outlook.com.br>
2022-05-29 17:47:01 -0300

    Adjusting text formating

--- main.py
+++ main.py
@@ -9,15 +9,8 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(options=chrome_options)
 
-
-
-
 googleFlight = GoogleFlights.GoogleFlightController(driver)
 googleFlight.goToHome()
 googleFlight.setFrom(config.fromAirport)
 googleFlight.setTo(config.toAirport)
 googleFlight.setDates(config.departureDate,config.returnDate)
-
-#googleFlight.searchFlights()
-print('done')
-#googleFlight.closeBrowser()
